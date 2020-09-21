555f95c9ff27c79f99de73f6b8fb8ea7a3456f42
Inari Listenmaa <inari.listenmaa@gmail.com>
2020-09-21 18:50:06 +0200

    Update filepaths and GHC version

--- NLP/WordNet/Consts.hs
+++ NLP/WordNet/Consts.hs
@@ -12,13 +12,13 @@ defaultBin :: IsString a => a
 
 #if defined (UNIX)
 dictDir         = "/dict"
-defaultPath     = "/usr/local/WordNet-2.0/dict"
-defaultBin      = "/usr/local/WordNet-2.0/bin"
+defaultPath     = "/usr/local/WordNet-3.0/dict"
+defaultBin      = "/usr/local/WordNet-3.0/bin"
 
 #elif defined (PC)
 dictDir         = "\\dict"
-defaultPath	= "c:\\WordNet 2.0\\dict"
-defaultBin      = "c:\\WordNet 2.0\\bin"
+defaultPath	= "c:\\WordNet 3.0\\dict"
+defaultBin      = "c:\\WordNet 3.0\\bin"
 
 #elif defined (MAC)
 dictDir         = ":Database"
@@ -28,7 +28,7 @@ defaultBin      = ":"
 #else
 -- guess unix style
 dictDir         = "/dict"
-defaultPath     = "/usr/local/WordNet-2.0/dict"
-defaultBin      = "/usr/local/WordNet-2.0/bin"
+defaultPath     = "/usr/local/WordNet-3.0/dict"
+defaultBin      = "/usr/local/WordNet-3.0/bin"
 
 #endif
